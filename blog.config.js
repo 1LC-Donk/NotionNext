dea3eca8fc135048c53409c438c061880ee7f823
1LC-Donk <olc.donk@gmail.com>
2025-07-18 20:47:35 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -14,7 +14,7 @@ const BLOG = {
   APPEARANCE: process.env.NEXT_PUBLIC_APPEARANCE || 'light', // ['light', 'dark', 'auto'], // light 日间模式 ， dark夜间模式， auto根据时间和主题自动夜间模式
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '1LC.Donk', //
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '1LC.Poker', //
   BIO: process.env.NEXT_PUBLIC_BIO || '1LC_Poker Group', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.youtube.com/@1LC_Poker', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '深深水下, OneLastChip, 1LC, 深水学院', // 网站关键词 英文逗号隔开
